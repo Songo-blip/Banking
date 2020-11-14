7b6ba1ab5a908ef309536422b38a512af506d082
Songo-blip <70211574+Songo-blip@users.noreply.github.com>
2020-11-14 22:23:00 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Banking;
+
+namespace Banks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank songo = new Bank("Songo", 7222, "Strand");
+            Customer myNewCustomer = new Customer("8198373764", "city of cape town ", "Sipho", "Administrator");
+            songo.AddCustomer(myNewCustomer);
+
+            var account = myNewCustomer.ApplyForBankAccount();
+            account.DepositMoney(20000, DateTime.Now, "Salary");
+
+            try
+            {
+                account.WithdrawMoney(300, DateTime.Now, "Buy Grossaries");
+                account.WithdrawMoney(2000, DateTime.Now, "Pay Rent");
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message + " contact administrator.");
+            }
+
+            Console.WriteLine("Current Balance :" + account.Balance);
+
+            var history = account.GetTransactionHistory();
+
+            Console.WriteLine(history);
+        }
+    }
+}
